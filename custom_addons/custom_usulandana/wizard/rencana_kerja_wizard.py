01995c14bf1180d99b1be800fb3a6a8dbdaf3399
William Purba <107050900+imwepe@users.noreply.github.com>
2026-05-20 16:07:55 +0700

    [FIX] Revisi proses cek tax dan setup pph

--- custom_addons/custom_usulandana/wizard/rencana_kerja_wizard.py
+++ custom_addons/custom_usulandana/wizard/rencana_kerja_wizard.py
@@ -1,38 +1,6 @@
-from odoo import models,fields,api
+from odoo import models, fields, api, exceptions
+from datetime import timedelta
 
-class RencanaKerjaWizard(models.TransientModel):
-    _name = 'rencana.kerja.wizard'
-
-    line_ids = fields.One2many(
-        'rencana.kerja.wizard.line',
-        'wizard_id',
-        string="Rencana Kerja"
-    )
-
-    def action_confirm(self):
-        self.ensure_one()
-
-        up_country = self.env['usulan.up.country'].browse(
-            self.env.context.get('active_id')
-        )
-
-        # hapus data lama dulu supaya tidak duplicate
-        up_country.rencana_kerja_line_ids.unlink()
-
-        # create ulang dari wizard
-        up_country.write({
-            'rencana_kerja_line_ids': [
-                (0, 0, {
-                    'tanggal': l.tanggal,
-                    'jenis_id': l.jenis_id.id,
-                    'tipe_visit_id': l.tipe_visit_id.id,
-                    'tujuan_id': l.tujuan_id.id,
-                    'keterangan': l.keterangan,
-                }) for l in self.line_ids
-            ]
-        })
-
-        return {'type': 'ir.actions.act_window_close'}
 
 class RencanaKerjaWizardLine(models.TransientModel):
     _name = 'rencana.kerja.wizard.line'
@@ -58,3 +26,69 @@ class RencanaKerjaWizardLine(models.TransientModel):
 
     deskripsi = fields.Text()
     tanggal = fields.Datetime(string="Tanggal")
+
+class RencanaKerjaWizard(models.TransientModel):
+    _name = 'rencana.kerja.wizard'
+
+    line_ids = fields.One2many(
+        'rencana.kerja.wizard.line',
+        'wizard_id',
+        string="Rencana Kerja"
+    )
+
+    def action_confirm(self):
+        self.ensure_one()
+
+        up_country = self.env['usulan.up.country'].browse(
+            self.env.context.get('active_id')
+        )
+
+        if not up_country.tgl_depart or not up_country.tgl_pulang:
+            raise exceptions.UserError(
+                "Tanggal keberangkatan dan tanggal pulang harus diisi."
+            )
+
+        tanggal_wajib = []
+        current_date = up_country.tgl_depart
+
+        while current_date <= up_country.tgl_pulang:
+            tanggal_wajib.append(current_date)
+            current_date += timedelta(days=1)
+
+        tanggal_rencana = set(
+            line.tanggal.date()
+            for line in self.line_ids
+            if line.tanggal
+        )
+        tanggal_kurang = []
+
+        for tanggal in tanggal_wajib:
+            if tanggal not in tanggal_rencana:
+                tanggal_kurang.append(
+                    tanggal.strftime('%d-%m-%Y')
+                )
+
+        if tanggal_kurang:
+            raise exceptions.UserError(
+                "Rencana kerja belum lengkap.\n\n"
+                "Tanggal berikut belum memiliki agenda:\n- "
+                + "\n- ".join(tanggal_kurang)
+            )
+
+        # hapus data lama dulu supaya tidak duplicate
+        up_country.rencana_kerja_line_ids.unlink()
+
+        # create ulang dari wizard
+        up_country.write({
+            'rencana_kerja_line_ids': [
+                (0, 0, {
+                    'tanggal': l.tanggal,
+                    'jenis_id': l.jenis_id.id,
+                    'tipe_visit_id': l.tipe_visit_id.id,
+                    'tujuan_id': l.tujuan_id.id,
+                    'deskripsi': l.deskripsi,
+                }) for l in self.line_ids
+            ]
+        })
+
+        return {'type': 'ir.actions.act_window_close'}
